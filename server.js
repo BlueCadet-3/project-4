ab4cfe30e27fa9606c88c4d021dc47239a80ae94
BlueCadet-3 <martysilha@gmail.com>
2021-01-26 12:57:28 -0600

    create directories and files

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 
+// Always require and configure near the top
+require("dotenv").config();
+
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
@@ -19,4 +22,14 @@ const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// Put API routes here, before the "catch all" route
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
